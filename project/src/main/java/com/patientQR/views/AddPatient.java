eef366f8d0382022123b4d1836c3665fde6eb835
Ramzi <ramzibenmansour.pro@gmail.com>
2023-12-06 04:49:03 +0100

    New panels added to AddPatient view

--- project/src/main/java/com/patientQR/views/AddPatient.java
+++ project/src/main/java/com/patientQR/views/AddPatient.java
@@ -4,192 +4,345 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Enumeration;
 import java.util.List;
 
-
-public class AddPatient extends Styling{
-    private JFrame frame;
-    private JPanel cards;
-    private CardLayout cardLayout;
-    private JPanel personalInfoPanel;
-    private JPanel radiologyPanel;
-    private JPanel medicalAnalysisPanel;
-    private JPanel chronicDiseasePanel;
-    private JPanel allergyPanel;
-
-    private JButton nextButton;
-    private JButton previousButton;
-    private JButton languageButton;
-
-    private JLabel titleLabel = new JLabel();
-    private JLabel usernameLabel;
-    private JLabel dobLabel;
-
-    private JTextField firstNameField;
-    private JTextField lastNameField;
-    private JTextField addressField;
-    private JTextField phoneNumberField;
-    private JTextField socialSecurityField;
-    private JTextField bloodGroupField;
-    private JTextField lifestyleField;
-    private JTextField familyHistoryField;
-
-    private JComboBox<String> genderComboBox;
-
-    private JCheckBox tobaccoCheckBox;
-    private JCheckBox alcoholCheckBox;
-    private JCheckBox drugsCheckBox;
-
-    private JTextArea chronicDiseaseTextArea;
-    private JTextArea medicationTextArea;
-
-    private JRadioButton yesAllergyRadio;
-    private JRadioButton noAllergyRadio;
-    private ButtonGroup allergyButtonGroup;
-
-    private JTextArea allergyTextArea;
-
-    private JFileChooser fileChooser;
-
-    private List<String> radiologyExams;
-    private List<String> medicalAnalyses;
-    private List<String> chronicDiseases;
-    private List<String> medicationAllergies;
+public class AddPatient {
+	private JFrame frame;
+	private JPanel examsListPanel = new JPanel();
+	private JPanel diseasesListPanel = new JPanel();
+	private JTextField diseaseNameField;
+	private JComboBox<String> examTypeComboBox;
+    private JTextField examFileField;
+    private SpinnerDateModel model;
     
-    private int currentCard = 0;
-    private JPanel panel;
-    
-    private Color backgroundColor = Styling.getBackgroundColor();
-    private Color textColor = Styling.getTextColor();
-
-
-    public AddPatient(){
-
-        frame = new JFrame("Add Patient");
-        frame.setSize(600, 400);
+    public AddPatient() {
+        frame = new JFrame("Ajouter un patient");
+        frame.setSize(900, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
 
-        panel = new JPanel();
-        cardLayout = new CardLayout();
-        panel.setLayout(cardLayout);
-        
-        frame.setLocationRelativeTo(null);
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
-        personalInfoPanel = createPersonalInfoPanel(); 
-        frame.getContentPane().setBackground(backgroundColor);
-        titleLabel.setForeground(textColor);
+        JLabel titleLabel = new JLabel("Informations personnelles");
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 18));
+        titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        mainPanel.add(titleLabel, BorderLayout.NORTH);
 
-        
+        JPanel formPanel = new JPanel(new GridLayout(0, 2, 10, 10));
 
-        radiologyExams = new ArrayList<>();
-        medicalAnalyses = new ArrayList<>();
-        chronicDiseases = new ArrayList<>();
-        medicationAllergies = new ArrayList<>();
-        
-        panel.add(personalInfoPanel, "personalInfo");
+        JTextField nameField = new JTextField(); // Create fields to hold user input
+        JTextField dobField = new JTextField();
+        JTextField addressField = new JTextField();
+        JComboBox<String> genderComboBox = new JComboBox<>(new String[]{"", "Homme", "Femme"});
+        JTextField phoneField = new JTextField();
+        JTextField socialSecurityField = new JTextField();
+        JComboBox<String> bloodGroupField = new JComboBox<>(new String[]{"", "A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"});
+        JTextField lifestyleField = new JTextField();
+        JTextField riskFactorsField = new JTextField();
 
-        frame.add(panel, BorderLayout.CENTER);
+        formPanel.add(new JLabel("Nom et Prénom:"));
+        formPanel.add(nameField);
+
+        formPanel.add(new JLabel("Date de naissance:"));
+        formPanel.add(dobField);
+
+        formPanel.add(new JLabel("Adresse:"));
+        formPanel.add(addressField);
+
+        formPanel.add(new JLabel("Sexe:"));
+        formPanel.add(genderComboBox);
+
+        formPanel.add(new JLabel("Numéro de téléphone:"));
+        formPanel.add(phoneField);
+
+        formPanel.add(new JLabel("Numéro de sécurité sociale:"));
+        formPanel.add(socialSecurityField);
+
+        formPanel.add(new JLabel("Groupe sanguin:"));
+        formPanel.add(bloodGroupField);
+
+        formPanel.add(new JLabel("Mode de vie:"));
+        formPanel.add(lifestyleField);
+
+        formPanel.add(new JLabel("Facteurs de risques:"));
+        formPanel.add(riskFactorsField);
+
+        mainPanel.add(formPanel, BorderLayout.CENTER);
 
         JButton nextButton = new JButton("Next");
         nextButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                currentCard++;
-                if (currentCard < panel.getComponentCount()) {
-                    cardLayout.show(panel, "panel" + currentCard);
-                } else {
-                    // savePatientData();
-                    JOptionPane.showMessageDialog(frame, "Patient information submitted!");
-                }
+                frame.getContentPane().removeAll();
+                frame.add(createRadiologyExamsPanel());
+                frame.revalidate();
+                frame.repaint();
             }
         });
 
-        frame.add(nextButton, BorderLayout.SOUTH);
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        buttonPanel.add(nextButton);
+        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+
+        frame.add(mainPanel);
         frame.setVisible(true);
     }
 
-    private JPanel createPersonalInfoPanel() {
-        JPanel personalInfoPanel = new JPanel(new GridBagLayout());
-        personalInfoPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(5, 5, 5, 5);
-        gbc.anchor = GridBagConstraints.WEST;
-        gbc.gridx = 0;
-        gbc.gridy = GridBagConstraints.RELATIVE;
+	private JPanel createRadiologyExamsPanel() {
+        JPanel radiologyExamsPanel = new JPanel(new BorderLayout());
+        radiologyExamsPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
-        JLabel titleLabel = new JLabel("Informations personnelles");
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 18));
-        titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        gbc.gridwidth = 2;
-        personalInfoPanel.add(titleLabel, gbc);
+        JPanel addExamPanel = new JPanel();
+        addExamPanel.setLayout(new BoxLayout(addExamPanel, BoxLayout.Y_AXIS));
 
-        addLabelAndField("Nom et Prénom:", new JTextField(), personalInfoPanel, gbc);
-        addLabelAndField("Date de naissance:", new JTextField(), personalInfoPanel, gbc);
-        addLabelAndField("Adresse:", new JTextField(), personalInfoPanel, gbc);
-        addLabelAndField("Sexe:", new JComboBox<>(new String[]{"", "Homme", "Femme"}), personalInfoPanel, gbc);
-        addLabelAndField("Numéro de téléphone:", new JTextField(), personalInfoPanel, gbc);
-        addLabelAndField("Numéro de sécurité sociale:", new JTextField(), personalInfoPanel, gbc);
-        addLabelAndField("Groupe sanguin:", new JComboBox<>(new String[]{"", "A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"}), personalInfoPanel, gbc);
-        addLabelAndField("Mode de vie:", new JTextField(), personalInfoPanel, gbc);
-        addLabelAndField("Facteurs de risques:", new JTextField(), personalInfoPanel, gbc);
+        JLabel titleLabel = new JLabel("Examens de radiologie");
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        addExamPanel.add(titleLabel);
 
-        JButton languageButton = new JButton("Toggle Language");
-        languageButton.addActionListener(e -> toggleLanguage());
-        gbc.gridwidth = 2;
-        gbc.gridx = 0;
-        personalInfoPanel.add(languageButton, gbc);
+        JPanel inputPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
-        return personalInfoPanel;
-    }
+        examTypeComboBox = new JComboBox<>(new String[]{"", "Radiographie", "TDM", "IRM", "Scintigraphie"});
+        examFileField = new JTextField(20);
 
-    private void addLabelAndField(String labelText, JComponent component, JPanel panel, GridBagConstraints gbc) {
-        JLabel label = new JLabel(labelText);
-        gbc.gridwidth = 1;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        panel.add(label, gbc);
-        gbc.gridx = 1;
-        gbc.fill = GridBagConstraints.NONE;
-        panel.add(component, gbc);
-    }
+        JButton importButton = new JButton("Sélectionner un fichier PDF");
+        importButton.addActionListener(e -> {
+            JFileChooser fileChooser = new JFileChooser();
+            int option = fileChooser.showOpenDialog(radiologyExamsPanel);
 
+            if (option == JFileChooser.APPROVE_OPTION) {
+                File selectedFile = fileChooser.getSelectedFile();
+                String filePath = selectedFile.getAbsolutePath();
+                examFileField.setText(filePath);
+            }
+        });
 
+        model = new SpinnerDateModel();
+        JSpinner dateSpinner = new JSpinner(model);
+        JSpinner.DateEditor editor = new JSpinner.DateEditor(dateSpinner, "dd/MM/yyyy");
+        dateSpinner.setEditor(editor);
 
+        JButton addButton = new JButton("Ajouter un examen");
+        addButton.addActionListener(e -> {
+            String examType = (String) examTypeComboBox.getSelectedItem();
+            String examFile = examFileField.getText();
+            String importDate = editor.getFormat().format(model.getDate());
+            addNewExam(examType, examFile, importDate);
+            clearFields();
+        });
 
-    private JPanel createRadiologyExamsPanel() {
-        JPanel radiologyExamsPanel = new JPanel(new GridLayout(5, 1));
+        inputPanel.add(new JLabel("Type: "));
+        inputPanel.add(examTypeComboBox);
+        inputPanel.add(new JLabel("Fichier: "));
+        inputPanel.add(examFileField);
+        inputPanel.add(importButton);
+        inputPanel.add(new JLabel("Date: "));
+        inputPanel.add(dateSpinner);
+        inputPanel.add(addButton);
+
+        addExamPanel.add(inputPanel);
+        radiologyExamsPanel.add(addExamPanel, BorderLayout.NORTH);
+
+        examsListPanel = new JPanel();
+        examsListPanel.setLayout(new BoxLayout(examsListPanel, BoxLayout.Y_AXIS));
+        JScrollPane scrollPane = new JScrollPane(examsListPanel);
+        radiologyExamsPanel.add(scrollPane, BorderLayout.CENTER);
+
+        JButton nextButton = new JButton("Suivant");
+        nextButton.addActionListener(e -> {
+            frame.getContentPane().removeAll();
+            frame.add(createMedicalAnalysesPanel());
+            frame.revalidate();
+            frame.repaint();
+        });
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        buttonPanel.add(nextButton);
+        radiologyExamsPanel.add(nextButton, BorderLayout.SOUTH);
 
         return radiologyExamsPanel;
+    }
+
+    private void addNewExam(String examType, String examFile, String importDate) {
+        JPanel examPanel = new JPanel();
+        examPanel.setLayout(new BoxLayout(examPanel, BoxLayout.Y_AXIS));
+
+        JLabel examLabel = new JLabel("Type: " + examType);
+        JLabel fileLabel = new JLabel("Fichier: " + examFile);
+        JLabel dateLabel = new JLabel("Date: " + importDate);
+
+        examPanel.add(examLabel);
+        examPanel.add(fileLabel);
+        examPanel.add(dateLabel);
+        examPanel.setBorder(BorderFactory.createEtchedBorder());
+
+        examsListPanel.add(examPanel);
+        examsListPanel.revalidate();
+        examsListPanel.repaint();
+    }
+
+    private void clearFields() {
+        diseaseNameField.setText("");
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(0, Calendar.JANUARY, 0);
+        model.setValue(calendar.getTime());
+
     }
 
     private JPanel createMedicalAnalysesPanel() {
         JPanel medicalAnalysesPanel = new JPanel(new GridLayout(5, 1));
 
+        JLabel titleLabel = new JLabel("Analyses médicales");
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 18));
+        titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        medicalAnalysesPanel.add(titleLabel); // Adding the title label
+
+        JButton nextButton = new JButton("Suivant");
+        nextButton.addActionListener(e -> {
+            frame.getContentPane().removeAll();
+            frame.add(createChronicDiseasesPanel());
+            frame.revalidate();
+            frame.repaint();
+        });
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        buttonPanel.add(nextButton);
+        medicalAnalysesPanel.add(nextButton); // Add the button to the panel
+
         return medicalAnalysesPanel;
     }
 
-    private JPanel createChronicDiseasesPanel() {
-        JPanel chronicDiseasesPanel = new JPanel(new GridLayout(5, 1));
 
-        return chronicDiseasesPanel;
-    }
+	
 
-    private JPanel createMedicationAllergiesPanel() {
-        JPanel medicationAllergiesPanel = new JPanel(new GridLayout(5, 1));
+	private JPanel createChronicDiseasesPanel() {
+	    JPanel chronicDiseasesPanel = new JPanel(new BorderLayout());
+	    chronicDiseasesPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
-        return medicationAllergiesPanel;
-    }
+	    JPanel addDiseasePanel = new JPanel();
+	    addDiseasePanel.setLayout(new BoxLayout(addDiseasePanel, BoxLayout.Y_AXIS));
 
-    
-    private void toggleLanguage() {
-    }
+	    JLabel titleLabel = new JLabel("Maladie chronique");
+	    titleLabel.setFont(new Font("Arial", Font.BOLD, 16));
+	    titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+	    addDiseasePanel.add(titleLabel);
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new AddPatient();
-            }
+	    JPanel inputPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+	    diseaseNameField = new JTextField(20);
+
+	    JButton addButton = new JButton("Ajouter une maladie");
+	    addButton.addActionListener(e -> {
+	        String diseaseName = diseaseNameField.getText();
+	        addNewDisease(diseaseName);
+	        clearFields();
+	    });
+
+	    inputPanel.add(new JLabel("La maladie:"));
+	    inputPanel.add(diseaseNameField);
+	    inputPanel.add(addButton);
+
+	    addDiseasePanel.add(inputPanel);
+	    chronicDiseasesPanel.add(addDiseasePanel, BorderLayout.NORTH);
+
+	    diseasesListPanel = new JPanel();
+	    diseasesListPanel.setLayout(new BoxLayout(diseasesListPanel, BoxLayout.Y_AXIS));
+	    JScrollPane scrollPane = new JScrollPane(diseasesListPanel);
+	    chronicDiseasesPanel.add(scrollPane, BorderLayout.CENTER);
+
+	    JButton nextButton = new JButton("Suivant");
+	    nextButton.addActionListener(e -> {
+	        frame.getContentPane().removeAll();
+	        frame.add(createAllergiesPanel());
+	        frame.revalidate();
+	        frame.repaint();
+	    });
+	    JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        buttonPanel.add(nextButton);
+	    chronicDiseasesPanel.add(nextButton, BorderLayout.SOUTH);
+
+	    return chronicDiseasesPanel;
+	}
+
+	private void addNewDisease(String diseaseName) {
+	    JPanel diseasePanel = new JPanel();
+	    diseasePanel.setLayout(new BoxLayout(diseasePanel, BoxLayout.Y_AXIS));
+
+	    JLabel diseaseLabel = new JLabel("Disease Name: " + diseaseName);
+
+	    diseasePanel.add(diseaseLabel);
+	    diseasePanel.setBorder(BorderFactory.createEtchedBorder());
+
+	    diseasesListPanel.add(diseasePanel);
+	    diseasesListPanel.revalidate();
+	    diseasesListPanel.repaint();
+	}
+
+	
+	private JPanel createAllergiesPanel() {
+	    JPanel allergiesPanel = new JPanel(new GridLayout(5, 1));
+
+	    JLabel titleLabel = new JLabel("Allergie médicamenteuses");
+	    titleLabel.setFont(new Font("Arial", Font.BOLD, 18));
+	    titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+	    allergiesPanel.add(titleLabel);
+
+	    JButton nextButton = new JButton("Suivant");
+	    nextButton.addActionListener(e -> {
+	        frame.getContentPane().removeAll();
+	        frame.add(recapitulatifPanel());
+	        frame.revalidate();
+	        frame.repaint();
+	    });
+	    JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        buttonPanel.add(nextButton);
+	    allergiesPanel.add(nextButton, BorderLayout.SOUTH);
+	    
+		return allergiesPanel;
+	}
+	
+	
+	private JPanel recapitulatifPanel() {
+        JPanel recapitulatifPanel = new JPanel(new BorderLayout());
+        recapitulatifPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+        JLabel titleLabel = new JLabel("Recapitulatif");
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 18));
+        titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        recapitulatifPanel.add(titleLabel, BorderLayout.NORTH);
+
+        JPanel infoPanel = new JPanel(new GridLayout(0, 2, 10, 10));
+
+        JScrollPane scrollPane = new JScrollPane(infoPanel);
+        recapitulatifPanel.add(scrollPane, BorderLayout.CENTER);
+
+        JButton validateButton = new JButton("Valider");
+        validateButton.addActionListener(e -> {
+            JOptionPane.showMessageDialog(frame, "Validation successful!", "Validation", JOptionPane.INFORMATION_MESSAGE);
         });
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        buttonPanel.add(validateButton);
+        recapitulatifPanel.add(buttonPanel, BorderLayout.SOUTH);
+
+        return recapitulatifPanel;
     }
+
+	
+
+	private void toggleLanguage() {
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new AddPatient();
+			}
+		});
+	}
 }
